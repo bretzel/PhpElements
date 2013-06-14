a9a3dbd76c753778483bdd67d8e78e14d1d60045
bretzel <lussier.serge@gmail.com>
2013-06-14 11:16:31 -0400

    tests and adaptations...

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+//$ipath = get_include_path() . PATH_SEPARATOR . "php" . PATH_SEPARATOR . "mastermenu" . PATH_SEPARATOR . "contents";
+//set_include_path($ipath);
+require_once 'services.php';
+?>
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <title>&& JSR-0XFFD2</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400italic,700|Open+Sans+Condensed:300,700" rel="stylesheet" />
+        <script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/jui.js"></script>
+        <script type="text/javascript" src="js/sh_main.js"></script>
+        <script type="text/javascript" src="js/sh_cpp.js"></script>
+        <script src="js/ejs.js"></script>
+        <link href="css/doxy.css" type="text/css" rel="stylesheet" />
+        <link rel="stylesheet" href="css/jui.css" />
+        <link rel="stylesheet" href="css/sh_ide-kdev.css" />
+        <link rel="stylesheet" href="css/style.css" />
+
+    </head>
+    <body>
+        
+        
+    </body>
+</html>
